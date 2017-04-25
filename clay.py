c5f0a371cb3b6d7412e0ba1cef9caa83fa91913c
Seth-Kaine <steve.monnard@gmail.com>
2017-04-25 10:38:50 +0200

    led alternate
    led on GPIO23

--- clay.py
+++ clay.py
@@ -5,18 +5,33 @@ from time import sleep
 print(">>>>>>> importation passed! >>>>>>>>>")
 # Needs to be BCM. GPIO.BOARD lets you address GPIO ports by periperal
 # connector pin number, and the LED GPIO isn't on the connector
+ledpin = 23
+
+
 GPIO.setmode(GPIO.BCM)
 
 # set up GPIO output channel
-GPIO.setup(16, GPIO.OUT)
-
-# On
-GPIO.output(16, GPIO.LOW)
-
-print(">>>>>>> GPIO 16 ON >>>>>>>>>")
-# Wait a bit
-sleep(10)
+GPIO.setup(ledpin, GPIO.OUT)
 
 # Off
-GPIO.output(16, GPIO.HIGH)
-print(">>>>>>> GPIO 16 OFF >>>>>>>>>")
+GPIO.output(ledpin, GPIO.LOW)
+
+print(">>>>>>> GPIO GPIO23 OFF >>>>>>>>>")
+# Wait a bit
+sleep(1)
+
+# On
+GPIO.output(ledpin, GPIO.HIGH)
+print(">>>>>>> GPIO GPIO23 ON >>>>>>>>>")
+
+# Off
+sleep(2)
+GPIO.output(ledpin, GPIO.LOW)
+
+print(">>>>>>> GPIO GPIO23 OFF >>>>>>>>>")
+# Wait a bit
+sleep(1)
+
+# On
+GPIO.output(ledpin, GPIO.HIGH)
+print(">>>>>>> GPIO GPIO23 ON >>>>>>>>>")
