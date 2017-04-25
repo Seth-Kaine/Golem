d52633b19a6d13c253fb6f9e0e4707a30d90f793
Seth-Kaine <steve.monnard@gmail.com>
2017-04-25 10:14:50 +0200

    print
    show in console :
    
    print(">>>>>>> GPIO 16 ON >>>>>>>>>")

--- clay.py
+++ clay.py
@@ -2,7 +2,7 @@
 
 import RPi.GPIO as GPIO
 from time import sleep
-
+print(">>>>>>> importation passed! >>>>>>>>>")
 # Needs to be BCM. GPIO.BOARD lets you address GPIO ports by periperal
 # connector pin number, and the LED GPIO isn't on the connector
 GPIO.setmode(GPIO.BCM)
@@ -13,8 +13,10 @@ GPIO.setup(16, GPIO.OUT)
 # On
 GPIO.output(16, GPIO.LOW)
 
+print(">>>>>>> GPIO 16 ON >>>>>>>>>")
 # Wait a bit
 sleep(10)
 
 # Off
 GPIO.output(16, GPIO.HIGH)
+print(">>>>>>> GPIO 16 OFF >>>>>>>>>")
